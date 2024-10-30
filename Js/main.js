a19cd7fd22784db98134e0a913e6a8115dd67a9e
Jeff <jeffrey.turner@cptc.edu>
2024-10-30 08:38:29 -0700

    Creating function stubs for add book form.

--- Js/main.js
+++ Js/main.js
@@ -6,3 +6,18 @@ myBook.price = 9.99;
 myBook.title = "Programming for Beginners";
 myBook.releaseDate = new Date(2023, 9, 8);
 console.log(myBook);
+window.onload = function () {
+    let addBookBtn = document.querySelector("#add-book");
+    addBookBtn.onclick = processBook;
+};
+function processBook() {
+    console.log("processBook was called");
+    let userBook = getBook();
+    if (userBook != null) {
+        addBook(userBook);
+    }
+}
+function getBook() {
+}
+function addBook(b) {
+}
--- ts/main.ts
+++ ts/main.ts
@@ -30,3 +30,39 @@ myBook.title = "Programming for Beginners";
 myBook.releaseDate = new Date(2023, 9, 8);  // Months are zero - indexed.
 
 console.log(myBook);
+
+window.onload = function() {
+    // Set up button click for add book form
+    let addBookBtn = document.querySelector("#add-book") as HTMLButtonElement;  //#finds the tag, "as" casts the element
+    addBookBtn.onclick = processBook;    
+}
+
+function processBook(){
+    console.log("processBook was called");
+
+    // Validation
+    let userBook = getBook();
+    if (userBook != null) {
+        addBook(userBook);
+    }
+}
+
+/**
+ * getBook will retrieve the book data from HTML
+ * If all data is valid a Book object will be
+ * returned.  If any data is invalid, null will 
+ * be returned.
+ */
+function getBook():Book {  // Return book or null.
+
+}
+
+/**
+ * Adds a Book object to web storage.  Assumes all
+ * data is valid.  Inside parenthesis is parameter
+ * and variable name.  :void is return type.
+ * @param b 
+ */
+function addBook(b:Book):void {
+
+}
